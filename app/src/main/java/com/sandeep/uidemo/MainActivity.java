fbe56530e3652d82634b8eb0c4c72e36388c7247
sandeeppadala <spadala88@gmail.com>
2019-02-05 11:22:53 +0530

    Added initial layout files

--- app/src/main/java/com/sandeep/uidemo/MainActivity.java
+++ app/src/main/java/com/sandeep/uidemo/MainActivity.java
@@ -1,7 +1,10 @@
 package com.sandeep.uidemo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void showLayoutView(View view) {
+        Intent intent=null;
+        switch (view.getId()) {
+            case R.id.btn_relative:
+                intent=new Intent(this,RelativeLayoutActivity.class);
+                break;
+            case R.id.btn_linaer:
+               intent=new Intent(this,LinearLayoutActivity.class);
+               break;
+            case R.id.btn_constraint:
+                intent=new Intent(this,ConstaintayoutActivity.class);
+                break;
+            case R.id.btn_table:
+                intent=new Intent(this,TableLayoutActivity.class);
+                break;
+            case R.id.btn_table_row:
+                intent=new Intent(this,tableRowLayoutActivity.class);
+                break;
+            case R.id.btn_frame:
+                intent=new Intent(this, FrameLayout.class);
+                break;
+
+        }
+        if(intent!=null) {
+        startActivity(intent);
+        }
+    }
+
+
+
 }
